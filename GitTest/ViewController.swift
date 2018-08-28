3aaa91c0616645c11b7264ae7f2c0e7eac3a779a
Khang Duong <s3635116@rmit.edu.vn>
2018-08-28 11:51:02 +0700

    Initial Commit

---
+++ GitTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitTest
+//
+//  Created by Marie Tram Tran on 08/28/18.
+//  Copyright © 2018 s3635116. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
